4dc1f60a9da9a02752e451c26cff2a00cc15062e
componavt <andrew.krizhanovsky@gmail.com>
2016-09-29 17:17:24 +0300

    meaning_relation list pag

--- resources/views/dict/lemma/edit.blade.php
+++ resources/views/dict/lemma/edit.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
         <h1>{{ trans('navigation.lemmas') }}</h1>
-        <h2>{{ trans('messages.editing')}} {{ trans('dict.of_lemma')}} "{{ $lemma->lemma}}"</h2>
+        <h2>{{ trans('messages.editing')}} {{ trans('dict.of_lemma')}}: {{ $lemma->lemma}}</h2>
         <p><a href="{{ LaravelLocalization::localizeURL('/dict/lemma/'.$lemma->id) }}">{{ trans('messages.back_to_show') }}</a></p>
         
         {!! Form::model($lemma, array('method'=>'PUT', 'route' => array('lemma.update', $lemma->id))) !!}
