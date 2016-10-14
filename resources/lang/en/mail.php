7e168e6eb50c59036fe250731e242884c2e3e7cd
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2016-10-14 11:26:50 +0300

    deleted html code of links in mail templates

--- resources/lang/en/mail.php
+++ resources/lang/en/mail.php
@@ -10,7 +10,9 @@ return [
     |
     */
 
-    'account_activate' => 'To activate your account <a href=":url">go here</a>',
+//    'account_activate' => 'To activate your account <a href=":url">go here</a>',
+    'account_activate' => 'To activate your account go to link :url',
     'account_activation_subj' => 'Account activation',
-    'account_reminder' => 'To create a new password, go to <a href=":url">link</a>',
+//    'account_reminder' => 'To create a new password, go to <a href=":url">link</a>',
+    'account_reminder' => 'To create a new password, go to link :url',
 ];
--- resources/lang/ru/mail.php
+++ resources/lang/ru/mail.php
@@ -10,7 +10,8 @@ return [
     |
     */
 
-    'account_activate' => 'Для активации аккаунта пройдите по <a href=":url">ссылке</a>',
+//    'account_activate' => 'Для активации аккаунта пройдите по <a href=":url">ссылке</a>',
+    'account_activate' => 'Для активации аккаунта пройдите по ссылке :url',
     'account_activation_subj' => 'Активация аккаунта',
-    'account_reminder' => 'Для создания нового пароля пройдите по <a href=":url">ссылке</a>',
+    'account_reminder' => 'Для создания нового пароля пройдите по ссылке :url',
 ];
