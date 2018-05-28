2a95df519f664eb9197e8e17f3c35bd8dfe97005
lunata <nataly@krc.karelia.ru>
2018-05-28 20:49:51 +0300

    space

--- app/Http/Controllers/Dict/LemmaController.php
+++ app/Http/Controllers/Dict/LemmaController.php
@@ -898,7 +898,7 @@ class LemmaController extends Controller
         
         foreach($lemmas as $lemma) {
             $list[] = ['id'  => $lemma->id, 
-                       'text'=> $lemma->lemma. '('.$lemma->pos->name.')'];
+                       'text'=> $lemma->lemma. ' ('.$lemma->pos->name.')'];
         }
 
         return Response::json($list);
