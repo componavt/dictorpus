6cc8bccc598fb9911756ba52b46429f97a4d82af
lunata <nataly@krc.karelia.ru>
2019-04-25 02:12:56 +0300

    new escape consonants

--- app/Library/Grammatic/VepsName.php
+++ app/Library/Grammatic/VepsName.php
@@ -242,7 +242,7 @@ class VepsName
     
     //consonant after which the vowel escapes before
     public static function consSetEscapeV() {
-        return "pbfsšzžcčnlt"; // jgvkr
+        return "pfsšzžcčlt"; // jgvkrbdhmn
     }
     
     /*
