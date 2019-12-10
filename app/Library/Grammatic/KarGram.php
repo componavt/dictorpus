cdfd8dca6a5694bbb00e2ddd5077e5206d667c88
lunata <nataly@krc.karelia.ru>
2019-12-10 21:35:40 +0300

    z - is consonant

--- app/Library/Grammatic/KarGram.php
+++ app/Library/Grammatic/KarGram.php
@@ -18,7 +18,7 @@ use App\Models\Dict\PartOfSpeech;
 class KarGram
 {
     public static function isConsonant($letter) {
-        $consonants = ['p', 't', 'k', 's', 'h', 'j', 'v', 'l', 'r', 'm', 'n', 'č', 'd', 'g'];
+        $consonants = ['p', 't', 'k', 's', 'h', 'j', 'v', 'l', 'r', 'm', 'n', 'č', 'd', 'g', 'z'];
         if (in_array($letter, $consonants)) {
             return true;
         } 
