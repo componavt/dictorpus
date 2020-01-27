f0a8bcde0a0cd3630efad9ed141a04417c40bcc8
lunata <nataly@krc.karelia.ru>
2020-01-27 23:59:58 +0300

    r in illative base

--- app/Library/Grammatic/VepsName.php
+++ app/Library/Grammatic/VepsName.php
@@ -347,7 +347,7 @@ class VepsName
     
     //consonant after which the vowel escapes before
     public static function consSetEscapeV() {
-        return "dpfsšzžcčltk"; // jgvkrbdhm
+        return "rdpfsšzžcčltk"; // jgvkbdhm
     }
     
     /*
