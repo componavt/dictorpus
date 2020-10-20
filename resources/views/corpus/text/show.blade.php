bdeb6980b00165c5ccb9864b6b84b68155da7e2b
lunata <nataly@krc.karelia.ru>
2020-10-20 20:46:25 +0300

    +link

--- resources/views/corpus/text/show.blade.php
+++ resources/views/corpus/text/show.blade.php
@@ -20,8 +20,9 @@
             | @include('widgets.form.button._edit', ['route' => '/corpus/text/'.$text->id.'/edit'])
             | <a href="{{ LaravelLocalization::localizeURL('/corpus/text/'.$text->id.'/markup') }}{{$args_by_get}}">{{ trans('corpus.re-markup') }}</a>            
             | @include('widgets.form.button._delete', ['route' => 'text.destroy', 'args'=>['id' => $text->id]]) 
+            | <a href="{{ LaravelLocalization::localizeURL('/corpus/text/create') }}{{$args_by_get}}">{{ trans('messages.create_new_m') }}</a>
         @else
-            | {{ trans('messages.edit') }} | {{ trans('messages.delete') }}
+            | {{ trans('messages.edit') }} | {{ trans('messages.delete') }} | {{ trans('messages.create_new_m') }}
         @endif 
             | <a href="/corpus/text/{{ $text->id }}/history{{$args_by_get}}">{{ trans('messages.history') }}</a>
             | <a href="{{ LaravelLocalization::localizeURL('/help/text/show') }}">? {{ trans('navigation.help') }}</a>            
