64f7c97ddcf72572c737afc809007bc635eafabd
lunata <nataly@krc.karelia.ru>
2021-03-11 10:56:16 +0300

    reverse dict

--- resources/views/page/dict_selections.blade.php
+++ resources/views/page/dict_selections.blade.php
@@ -6,7 +6,6 @@
 
 @section('body')
     <p><a href="{{ LaravelLocalization::localizeURL('/dict/lemma/omonyms') }}">{{ trans('navigation.omonyms') }}</a></p>
-    <p><a href="{{ LaravelLocalization::localizeURL('/dict/reverse_lemma') }}">{{ trans('navigation.reverse_dictionary') }}</a></p>
     <p><a href="{{ LaravelLocalization::localizeURL('/dict/reverse_lemma/inflexion_groups') }}">{{ trans('navigation.inflexion_groups') }}</a></p>
     <p><a href="{{ LaravelLocalization::localizeURL('/dict/lemma_wordform/affix_freq') }}">{{ trans('navigation.affix_freq') }}</a></p>
     <p><a href="{{ LaravelLocalization::localizeURL('/dict/lemma_wordform/pos_common_wordforms') }}">{{ trans('navigation.pos_common_wordforms') }}</a></p>
