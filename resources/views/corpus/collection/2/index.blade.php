5bcb4fd553fcadde748cbc3d9c313b5f8edc31eb
lunata <nataly@krc.karelia.ru>
2022-02-03 19:54:51 +0300

    style

--- resources/views/corpus/collection/2/index.blade.php
+++ resources/views/corpus/collection/2/index.blade.php
@@ -4,6 +4,10 @@
 {{ trans('navigation.collections') }}
 @stop
 
+@section('headExtra')
+    {!!Html::style('css/text.css')!!}
+@stop
+
 @section('body')
     <h2>{{trans('collection.name_list')[$id]}}</h2>
 
@@ -11,11 +15,13 @@
     <h3>{{$genre->name_pl}}</h3>
     
         @foreach ($genre->plots as $plot)
+    <div class="subdiv">
     <h4>{{$plot->name}} ({{$plot->texts->count(0)}})</h4>
             @foreach ($plot->texts()->whereIn('lang_id', $lang_id)->get() as $text)
             @include('corpus.collection._text')
             @endforeach
         @endforeach
+    </div>
     @endforeach
 
 @stop
