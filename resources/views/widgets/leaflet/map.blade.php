c78fdb9634ef16d7e64cbcb15cb191a50eae427f
lunata <nataly@krc.karelia.ru>
2022-04-16 16:18:34 +0300

    small

--- resources/views/widgets/leaflet/map.blade.php
+++ resources/views/widgets/leaflet/map.blade.php
@@ -1,6 +1,6 @@
 <div style="display: flex; justify-content: space-between">
 @foreach ($legend as $color => $color_text)
-    <div><span class="marker-icon marker-{{$color}}">&nbsp;</span> {{$color_text}}</div>
+    <div style="padding-right: 10px"><span class="marker-icon marker-{{$color}}">&nbsp;</span> {{$color_text}}</div>
     @endforeach
 </div>    
-    <div id="mapid" style="margin-top: 20px; width: 100%; min-width: 750px; height: 1300px;"></div>
+    <div id="mapid" style="margin-top: 20px; width: 100%; height: 1300px;"></div>
