0e1bd457835c8241b98690f32fc17b12e9679a19
lunata <nataly@krc.karelia.ru>
2022-04-16 16:22:34 +0300

    small

--- resources/views/widgets/leaflet/map.blade.php
+++ resources/views/widgets/leaflet/map.blade.php
@@ -1,4 +1,4 @@
-<div style="display: flex; justify-content: space-between">
+<div style="display: flex; justify-content: space-between; flex-wrap:wrap">
 @foreach ($legend as $color => $color_text)
     <div style="padding-right: 10px"><span class="marker-icon marker-{{$color}}">&nbsp;</span> {{$color_text}}</div>
     @endforeach
