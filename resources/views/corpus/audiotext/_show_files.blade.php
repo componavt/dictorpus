db5cd7dc17100124fe6f92cb3fe3344977165a1f
lunata <nataly@krc.karelia.ru>
2022-04-18 01:47:43 +0300

    small

--- resources/views/corpus/audiotext/_show_files.blade.php
+++ resources/views/corpus/audiotext/_show_files.blade.php
@@ -1,8 +1,10 @@
 @foreach ($audiotexts as $audiotext)
     <div style='display:flex; margin-bottom: 20px'>
         @include('widgets.audio', ['route'=>$audiotext->url()])
+        @if (isset($action) && $action == 'edit')
         <div style="padding-left: 10px">
             <i class="fa fa-trash fa-lg" style="color: #972d1a; cursor: pointer" onClick="removeAudio({{$audiotext->text->id}}, {{$audiotext->id}})"></i>
         </div>
+        @endif
     </div>
 @endforeach
