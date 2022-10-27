39db96537ac1242ca04c8c57fc1c119aa9f9f757
lunata <nataly@krc.karelia.ru>
2022-10-27 10:41:45 +0300

    small

--- resources/views/header/header_olodict.blade.php
+++ resources/views/header/header_olodict.blade.php
@@ -3,9 +3,15 @@
                 <a href="/olodict">
                     <img src="/images/logo.png">
                 </a>
-                <a href="/olodict">
-                    {{trans('olodict.site_title')}}<br>{{trans('olodict.site_title_add')}}
+                <div>
+                <a href="/olodict" class='site-title'>
+                    {{trans('olodict.site_title')}}
                 </a>
+                <br>
+                <a href="/olodict" class='site-title-add'>
+                    {{trans('olodict.site_title_add')}}
+                </a>
+                </div>
             </div>
             <div class="menu">
                 <li><a href="{{ LaravelLocalization::localizeURL('/olodict/help')}}">{{trans('olodict.help')}}</a></li>
