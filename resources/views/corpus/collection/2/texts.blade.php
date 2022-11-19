bb648587643d0a01a44393076da8c6ba6e5dd074
lunata <nataly@krc.karelia.ru>
2022-11-19 12:40:48 +0300

    rune columns

--- resources/views/corpus/collection/2/texts.blade.php
+++ resources/views/corpus/collection/2/texts.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.page')
 
 @section('page_title')
-{{ trans('collection.name_list')[3] }}
+{{ trans('collection.name_list')[2] }}
 @stop
 
 @section('headExtra')
@@ -11,10 +11,7 @@
 
 @section('body')
     <p>
-        <a href="{{ LaravelLocalization::localizeURL('/corpus/collection/3') }}">{{trans('collection.to_collection')}}</a>
-        @if(isset($back_link)) 
-        | <a href="{{ LaravelLocalization::localizeURL($back_link[0]) }}">{{$back_link[1]}}</a>
-        @endif
+        <a href="{{ LaravelLocalization::localizeURL('/corpus/collection/2') }}">{{trans('collection.to_collection')}}</a>
     </p>
     
     <h2>{{$page_title}}</h2>
@@ -24,23 +21,18 @@
     <thead>
         <tr>
             <th>No</th>
-            <th>{{ trans('dict.dialect') }}</th>
             <th>{{ trans('corpus.title') }}</th>
             <th>{{ trans('messages.translation') }}</th>
+            <th>{{ trans('messages.year') }}</th>
+            <th>{{ trans('corpus.record_place') }}</th>
+            <th>{{ trans('corpus.informant') }}</th>
+            <th>{{ trans('corpus.recorder') }}</th>
         </tr>
     </thead>
     
         @foreach ($texts as $text)
         <tr>
             <td data-th="No">{{ $list_count++ }}</td>
-            <td data-th="{{ trans('dict.dialect') }}">
-                @if($text->dialects)
-                    @foreach ($text->dialects as $dialect)
-                    {{$dialect->name}}<br>
-                    @endforeach
-
-                @endif
-            </td>
             <td data-th="{{ trans('corpus.title') }}">
                 {{-- $text->authorsToString() ? $text->authorsToString().'.' : '' --}}
                 <a href="{{ LaravelLocalization::localizeURL('/corpus/text/'.$text->id.$url_args) }}">{{$text->title}}</a>
@@ -49,6 +41,26 @@
                 @if ($text->transtext)
                 {{-- $text->transtext->authorsToString() ? $text->transtext->authorsToString().'.' : '' --}}
                 {{$text->transtext->title}}
+                @endif
+            </td>
+            <td data-th="{{ trans('messages.year') }}">
+                @if ($text->event && $text->event->date)
+                    {{$text->event->date}}
+                @endif
+            </td>
+            <td data-th="{{ trans('corpus.record_place') }}">
+                @if ($text->event && $text->event->place)
+                    @include('corpus.place._to_string',['place' => $text->event->place])
+                @endif
+            </td>
+            <td data-th="{{ trans('corpus.informant') }}">
+                @if ($text->event && sizeof($text->event->informants))
+                    {{join(', ', $text->event->informantsWithLink())}}
+                @endif
+            </td>
+            <td data-th="{{ trans('corpus.recorder') }}">
+                @if ($text->event && sizeof($text->event->recorders))
+                    {{join(', ', $text->event->recordersWithLink())}}
                 @endif
             </td>
         </tr>
