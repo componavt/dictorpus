c0105ef1a743cef3fe2d65af8a46cc500277a6ba
lunata <nataly@krc.karelia.ru>
2023-04-16 20:55:38 +0300

    new map

--- resources/views/corpus/audiotext/map.blade.php
+++ resources/views/corpus/audiotext/map.blade.php
@@ -5,15 +5,15 @@
 @stop
 
 @section('headExtra')
- <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-   crossorigin=""/>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
       {!!Html::style('css/map.css')!!}
 @stop
 
 @section('body')
+    <h4>{{ trans('corpus.audio_map_info') }}</h4>
     @include('widgets.leaflet.map')
-    <p style='margin-top: 20px; font-size: 16px;'>{{ trans('corpus.audio_map_info') }}</p>
 @stop
 
 @section('footScriptExtra')
--- resources/views/widgets/leaflet/map_script.blade.php
+++ resources/views/widgets/leaflet/map_script.blade.php
@@ -1,13 +1,15 @@
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-   crossorigin=""></script>
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+     crossorigin=""></script>
     
 <script>
     var mymap = L.map('mapid').setView([62, 35], 7);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
             maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+/*            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',*/
             id: 'mapbox/streets-v11',
             tileSize: 512,
             zoomOffset: -1
