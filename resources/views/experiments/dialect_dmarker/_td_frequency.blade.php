95aaceccd7efb7be41abcdffaa706d8015dd745b
lunata <nataly@krc.karelia.ru>
2023-04-27 14:24:01 +0300

    s

--- resources/views/experiments/dialect_dmarker/_td_frequency.blade.php
+++ resources/views/experiments/dialect_dmarker/_td_frequency.blade.php
@@ -1,11 +1,9 @@
-<?php $align = 'style="text-align: '.($output=='frequency' ? 'right' : 'left').'"';?>
-
-            <td {{$align}}>
+            <td style="text-align: {{$output=='frequency' ? 'right' : 'left'}}">
                 <a href='/ru/corpus/sentence/results?search_dialect[]={{$dialect_id}}&search_words[1][w]="{{$template}}"'
                    style="text-decoration: none; color: {{ $variant['color'] }}" title="{{$dialect_info['name']}}">
                 {{ $variant['t_'.$output] }}
                 </a>
             </td>
-            <td {{$align}}>
+            <td style="text-align: {{$output=='frequency' ? 'right' : 'left'}}">
                 {{ $variant['w_'.$output] }}
             </td>
