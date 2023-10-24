bf7eb1adfc0624f3466cea3e87f6780c3f42e151
lunata <nataly@krc.karelia.ru>
2023-10-24 21:32:31 +0300

    small

--- resources/views/dict/audio/list/record.blade.php
+++ resources/views/dict/audio/list/record.blade.php
@@ -14,7 +14,8 @@
     <p><a href="{{ LaravelLocalization::localizeURL('corpus/informant/'.$informant->id).'/audio'}}">Вернуться к информанту</a>
     <p><a href="{{ LaravelLocalization::localizeURL('dict/audio/list/'.$informant->id) .'/voiced' }}">{{trans('dict.check_voiced_lemmas')}}</a> ({{$informant->audios()->count()}})</p>
     <p><a href="{{ LaravelLocalization::localizeURL('dict/audio/list/'.$informant->id) }}">{{trans('dict.edit_list_for_voicing')}}</a> ({{$informant->lemmas()->count()}})</p>
-    <p><a href="{{ LaravelLocalization::localizeURL('dict/audio/list/'.$informant->id) .'/choose'}}">{{trans('dict.add-lemmas-for-voicing')}}</a> ({{format_number($informant->unvoicedLemmasCount())}})</p>
+    <p><a href="{{ LaravelLocalization::localizeURL('dict/audio/list/'.$informant->id) .'/choose'}}">{{trans('dict.add-lemmas-for-voicing')}}</a> 
+        ({{ format_number($informant->unvoicedLemmasCount()) }} / {{ format_number($informant->unvoicedDialectLemmasCount()) }})</p>
 
     @include('dict.audio.list._record_block')
     <input type="hidden" id="informant_id" value="{{$informant->id}}">
