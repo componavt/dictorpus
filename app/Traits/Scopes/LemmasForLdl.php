d0e876fc8d508d9a992ebfb01db68ca00d49f4b0
lunata <nataly@krc.karelia.ru>
2023-11-08 12:53:14 +0300

    vepkar manual

--- app/Traits/Scopes/LemmasForLdl.php
+++ app/Traits/Scopes/LemmasForLdl.php
@@ -5,7 +5,13 @@ use App\Models\Dict\Label;
 trait LemmasForLdl
 {    
     public static function scopeForLdl($builder) {
-        return $builder->whereIn('id', Label::ldlLemmas());
+//        return $builder->whereIn('id', Label::ldlLemmas());
+        return $builder->whereIn('id', function ($q) {
+            $q->select('lemma_id')->from('meanings')
+              ->whereIn('id', function ($q2) {
+                  $q2->select('meaning_id')->from('meaning_place');
+              });
+        });
     }
 }    
 
