1c739e58d78ee8475998ade208327dd05b5b77bf
lunata <nataly@krc.karelia.ru>
2023-11-13 11:07:02 +0300

    small

--- resources/views/page/about_karelians.blade.php
+++ resources/views/page/about_karelians.blade.php
@@ -8,7 +8,7 @@
     <div class="text-with-photo">
         <div>{!! trans('page.about_karelians') !!}</div>
         <div class="photo-right-to-text">
-            <img src="/images/KV.PNG">
+            <img src="/images/KV.png">
             <div style="display:flex; margin-top: 20px;">
                 <span style="width:50px; height:40px; background-color:#ff7f27; margin-right: 20px;"></span>{!! trans('page.about_karelian_stats') !!}
             </div>
--- resources/views/page/about_veps.blade.php
+++ resources/views/page/about_veps.blade.php
@@ -8,7 +8,7 @@
     <div class="text-with-photo">
         <div>{!! trans('page.about_veps') !!}</div>
         <div class="photo-right-to-text">
-            <img src="/images/KV.PNG">
+            <img src="/images/KV.png">
             <div style="display:flex; margin-top: 20px;">
                 <span style="width:50px; height:40px; background-color:#cf101a; margin-right: 20px;"></span>{!! trans('page.about_veps_stats') !!}
             </div>
