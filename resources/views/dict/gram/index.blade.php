1710fce5c3eb59f9be12e5a9b4e8701d2054e8a8
lunata <nataly@krc.karelia.ru>
2025-04-01 17:24:47 +0300

    grams table

--- resources/views/dict/gram/index.blade.php
+++ resources/views/dict/gram/index.blade.php
@@ -6,48 +6,56 @@
 @stop
 
 @section('body')
-        <p style="text-align: right">
-        @if (User::checkAccess('ref.edit'))
-            <a href="{{ LaravelLocalization::localizeURL('/dict/gram/create') }}">
-        @endif
-            {{ trans('messages.create_new_m') }}
-        @if (User::checkAccess('ref.edit'))
-            </a>
-        @endif
-        </p>
+    <p style="text-align: right">
+    @if (User::checkAccess('ref.edit'))
+        <a href="{{ LaravelLocalization::localizeURL('/dict/gram/create') }}">
+    @endif
+        {{ trans('messages.create_new_m') }}
+    @if (User::checkAccess('ref.edit'))
+        </a>
+    @endif
+    </p>
         
-        <?php $column_num = 4;
-           $count = 1; ?>
-        <div class="row">
-        @foreach($grams as $name => $grams_list)
-            <div class="col-sm-3">
-                <h3>{{ $name }}</h3>
-                @foreach($grams_list as $gramzik)
-                <p>{{ $gramzik->sequence_number }}) 
+    <table class="table-bordered table-wide table-striped rwd-table wide-md">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>{{ trans('messages.in_english') }}</th>
+                <th>{{ trans('messages.in_russian') }}</th>
+                <th>LGR</th>
+                <th>Unimorph</th>
+                <th>CONLL</th>
+                @if (User::checkAccess('ref.edit'))
+                <th>{{ trans('messages.actions') }}</th>
+                @endif
+            </tr>
+        </thead>
+        <tbody>
+    @foreach($grams as $name => $grams_list)
+            <tr><td colspan="6" style="font-weight: bold">{{ $name }}</td></tr>
+        @foreach($grams_list as $gramzik)
+            <tr>
+                <td data-th="No">{{ $gramzik->sequence_number }}</td>
+                <td data-th="{{ trans('messages.in_english') }}">{{ $gramzik->name_en }}</td>
+                <td data-th="{{ trans('messages.in_russian') }}">
                     @if (User::checkAccess('ref.edit'))
                         <a href="{{ LaravelLocalization::localizeURL('/dict/gram/'.$gramzik->id) }}">{{ $gramzik->name }}</a> 
                     @else
                         {{ $gramzik->name }}
                     @endif
-{{--                    @if ($gramzik->{$short_name_column})
-                        ({{ $gramzik->{$short_name_column} }})
-                    @endif --}}
-                    @if ($gramzik->lgr)
-                        ({{ $gramzik->lgr }})
-                    @endif
-                    @if (User::checkAccess('ref.edit'))
-                        @include('widgets.form.button._edit', ['route' => '/dict/gram/'.$gramzik->id.'/edit', 'without_text' => 1])                        
-                    @endif
-                </p>
-                @endforeach
-            </div>
-            <?php if ($count==$column_num): ?>
-        </div>
-        <div class="row">
-            <?php endif; ?>
-            <?php $count++; ?>
+                </td>
+                <td data-th="LGR">{{ $gramzik->lgr }}</td>
+                <td data-th="Unimorph">{{ $gramzik->unimorph }}</td>
+                <td data-th="CONLL">{{ $gramzik->conll }}</td>
+            @if (User::checkAccess('ref.edit'))
+                <td data-th="{{ trans('messages.actions') }}">            
+                        @include('widgets.form.button._edit', ['route' => '/dict/gram/'.$gramzik->id.'/edit', 'without_text' => 1])    
+                </td>
+            @endif
         @endforeach
-        </div>
+    @endforeach
+        </tbody>
+    </table>
 @stop
 
 
