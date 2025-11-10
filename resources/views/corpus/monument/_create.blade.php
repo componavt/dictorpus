0faf4f33b5fc126ca9b0259f385dfb9344162d22
lunata <nataly@krc.karelia.ru>
2025-11-10 10:24:15 +0300

    small

--- resources/views/corpus/monument/_create.blade.php
+++ resources/views/corpus/monument/_create.blade.php
@@ -1,4 +1,5 @@
-        <p><a href="{{ LaravelLocalization::localizeURL('/corpus/monument/') }}">{{ trans('messages.back_to_list') }}</a></p>
+        <p>
+            <a href="{{ LaravelLocalization::localizeURL('/corpus/monument/') }}{{$args_by_get}}">{{ trans('messages.back_to_list') }}</a></p>
         
         {!! Form::open(array('method'=>'POST', 'route' => array('monument.store'))) !!}
         @include('corpus.monument._form_create_edit', ['submit_title' => trans('messages.create_new_m'),
--- resources/views/corpus/monument/_edit.blade.php
+++ resources/views/corpus/monument/_edit.blade.php
@@ -1,5 +1,7 @@
         <h2>{{ trans('messages.editing')}} {{ trans('corpus.of_monument')}} <span class='imp'>"{{ $monument->title}}"</span></h2>
-        <p><a href="{{ LaravelLocalization::localizeURL('/corpus/monument/'.$monument->id) }}">{{ trans('messages.back_to_show') }}</a></p>
+        <p>
+            <a href="{{ LaravelLocalization::localizeURL('/corpus/monument/') }}{{$args_by_get}}">{{ trans('messages.back_to_list') }}</a>
+            | <a href="{{ LaravelLocalization::localizeURL('/corpus/monument/'.$monument->id) }}{{$args_by_get}}">{{ trans('messages.back_to_show') }}</a></p>
         
         {!! Form::model($monument, array('method'=>'PUT', 'route' => array('monument.update', $monument->id))) !!}
         @include('corpus.monument._form_create_edit', ['submit_title' => trans('messages.save'),
