38c992d0d0f04ef095f3f0a4c1a027d3abf23f6f
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2026-03-18 09:56:07 +0300

    small

--- app/Http/Controllers/Corpus/CollectionController.php
+++ app/Http/Controllers/Corpus/CollectionController.php
@@ -22,11 +22,13 @@ use App\Models\Dict\Lang;
 
 class CollectionController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('corpus.collection.index');
     }
-    
-    public function show($id, Request $request) {
+
+    public function show($id, Request $request)
+    {
         $id = (int)$id;
         $for_print = (int)($request->for_print);
 
@@ -35,12 +37,13 @@ class CollectionController extends Controller
                 $author_id = Collection::getCollectionAuthors($id);
                 $author = Author::find($author_id);
                 $text_count = Text::whereIn('id', function ($q) use ($author_id) {
-                                    $q->select('text_id')->from('author_text')
-                                      ->whereIn('author_id', [$author_id]);
-                                })->count();
-                return view('corpus.collection.'.$id.'.index',
-                        compact('author', 'id', 'text_count'));
-                
+                    $q->select('text_id')->from('author_text')
+                        ->whereIn('author_id', [$author_id]);
+                })->count();
+                return view(
+                    'corpus.collection.' . $id . '.index',
+                    compact('author', 'id', 'text_count')
+                );
             } elseif (Collection::isCollectionByGenre($id)) {
                 $lang_ids = Collection::getCollectionLangs($id);
                 $langs = Lang::whereIn('id', $lang_ids)->orderBy('id')->get();
@@ -49,79 +52,98 @@ class CollectionController extends Controller
                     $genres = [Genre::find($genre_arr[0])];
                 } else {
                     $genres = Genre::where('parent_id', Collection::getCollectionGenres($id))
-                               ->orderBy('sequence_number')->get();
+                        ->orderBy('sequence_number')->get();
                     $genre_arr = Genre::find(Collection::getCollectionGenres($id))
-                            ->getSubGenreIds();
+                        ->getSubGenreIds();
                 }
                 $text_count = Text::whereIn('lang_id', $lang_ids)
-                                  ->whereIn('id', function ($q) use ($genre_arr) {
-                                    $q->select('text_id')->from('genre_text')
-                                      ->whereIn('genre_id', $genre_arr);
-                                })->count();
+                    ->whereIn('id', function ($q) use ($genre_arr) {
+                        $q->select('text_id')->from('genre_text')
+                            ->whereIn('genre_id', $genre_arr);
+                    })->count();
                 $dialects = [];
                 if ($id == 1) {
                     $dialects = Dialect::whereIn('lang_id', $lang_ids)->get();
                 } elseif ($id == 6) {
                     foreach ($genres as $genre) {
                         foreach ($langs as $lang) {
-                        $dials = Dialect::where('lang_id', $lang->id)->get();
+                            $dials = Dialect::where('lang_id', $lang->id)->get();
                             foreach ($dials as $dialect) {
                                 $texts = $dialect->textsByGenre($genre->id)->sortBy('title');
                                 if (!count($texts)) {
                                     continue;
                                 }
-                                $dialects[$genre->id]['langs'][$lang->id]['dialects'][$dialect->id] = ['dialect'=>$dialect, 'texts'=>$texts];
+                                $dialects[$genre->id]['langs'][$lang->id]['dialects'][$dialect->id] = ['dialect' => $dialect, 'texts' => $texts];
                             }
-                            $dialects[$genre->id]['langs'][$lang->id]['lang_text_count'] = 
-                                    Text::where('lang_id', $lang->id)
-                                      ->whereIn('id', function ($q) use ($genre) {
-                                        $q->select('text_id')->from('genre_text')
-                                          ->where('genre_id', $genre->id);
-                                    })->count();
-                        } 
-                        $dialects[$genre->id]['genre_text_count'] = 
-                                Text::whereIn('lang_id', $lang_ids)
-                                  ->whereIn('id', function ($q) use ($genre) {
+                            $dialects[$genre->id]['langs'][$lang->id]['lang_text_count'] =
+                                Text::where('lang_id', $lang->id)
+                                ->whereIn('id', function ($q) use ($genre) {
                                     $q->select('text_id')->from('genre_text')
-                                      ->where('genre_id', $genre->id);
+                                        ->where('genre_id', $genre->id);
                                 })->count();
+                        }
+                        $dialects[$genre->id]['genre_text_count'] =
+                            Text::whereIn('lang_id', $lang_ids)
+                            ->whereIn('id', function ($q) use ($genre) {
+                                $q->select('text_id')->from('genre_text')
+                                    ->where('genre_id', $genre->id);
+                            })->count();
                     }
-//dd($dialects);                    
+                    //dd($dialects);                    
                 }
-                return view('corpus.collection.'.$id.'.index',
-                        compact('dialects', 'for_print', 'genres', 'id', 'lang_ids', 
-                                'langs', 'text_count'));
+                return view(
+                    'corpus.collection.' . $id . '.index',
+                    compact(
+                        'dialects',
+                        'for_print',
+                        'genres',
+                        'id',
+                        'lang_ids',
+                        'langs',
+                        'text_count'
+                    )
+                );
             } elseif ($id == 7) {
                 return Redirect::to('/corpus/monument');
             }
         }
         return Redirect::to('/corpus/collection');
     }
-    
-    public function runeTopics(Request $request) {
+
+    public function runeTopics(Request $request)
+    {
         $for_print = (int)($request->for_print);
         $genres = Genre::where('parent_id', Collection::getCollectionGenres(2))
-                   ->orderBy('sequence_number')->get();
+            ->orderBy('sequence_number')->get();
         $genre_arr = Genre::find(Collection::getCollectionGenres(2))
-                ->getSubGenreIds();
+            ->getSubGenreIds();
         $lang_id = Collection::getCollectionLangs(2);
         $dialects = Dialect::whereIn('lang_id', $lang_id)->get();
-        return view('corpus.collection.2.topics',
-                compact('dialects', 'for_print', 'genres', 'lang_id'));        
+        return view(
+            'corpus.collection.2.topics',
+            compact('dialects', 'for_print', 'genres', 'lang_id')
+        );
     }
-    
-    public function runesForPlot($plot_id, Request $request) {
+
+    public function runesForPlot($plot_id, Request $request)
+    {
         $for_print = (int)($request->for_print);
         $plot = Plot::find($plot_id);
+        if (!$plot) {
+            return;
+        }
         $lang_id = Collection::getCollectionLangs(2);
         $texts = $plot->texts()->whereIn('lang_id', $lang_id)->get()->sortBy('year');
-        $page_title = trans('corpus.plot'). ': '. $plot->name;
-        $url_args = '?search_collection=2&search_plot='.$plot->id.'&for_print='.$for_print;
-        return view('corpus.collection.2.texts',
-                compact('for_print', 'lang_id', 'page_title', 'texts', 'url_args'));        
+        $page_title = trans('corpus.plot') . ': ' . $plot->name;
+        $url_args = '?search_collection=2&search_plot=' . $plot->id . '&for_print=' . $for_print;
+        return view(
+            'corpus.collection.2.texts',
+            compact('for_print', 'lang_id', 'page_title', 'texts', 'url_args')
+        );
     }
-    
-    public function runesForTopic($topic_id, Request $request) {
+
+    public function runesForTopic($topic_id, Request $request)
+    {
         $for_print = (int)($request->for_print);
         $topic = Topic::find($topic_id);
         $plot = Plot::find($request->plot_id);
@@ -130,49 +152,60 @@ class CollectionController extends Controller
         }
         $lang_id = Collection::getCollectionLangs(2);
         $texts = $topic->textsForPlot($plot->id)->whereIn('lang_id', $lang_id)->get()->sortBy('year');
-        $page_title = trans('corpus.plot'). ': '. $plot->name.'<br>'.trans('corpus.topic'). ': '. $topic->name;
-        $url_args = '?search_collection=2&search_topic='.$topic->id.'&for_print='.$for_print;
+        $page_title = trans('corpus.plot') . ': ' . $plot->name . '<br>' . trans('corpus.topic') . ': ' . $topic->name;
+        $url_args = '?search_collection=2&search_topic=' . $topic->id . '&for_print=' . $for_print;
         $back_link = ['/corpus/collection/2/topics', trans('collection.topic_index')];
-        return view('corpus.collection.2.texts',
-                compact('back_link', 'for_print', 'page_title', 'texts', 'url_args'));        
+        return view(
+            'corpus.collection.2.texts',
+            compact('back_link', 'for_print', 'page_title', 'texts', 'url_args')
+        );
     }
-    
-    public function predictionTextsForCycle($cycle_id) {
+
+    public function predictionTextsForCycle($cycle_id)
+    {
         $cycle = Cycle::find($cycle_id);
         $lang_id = Collection::getCollectionLangs(3);
         $texts = $cycle->texts()->whereIn('lang_id', $lang_id)->get();
-        $page_title = trans('corpus.cycle'). ': '. $cycle->name;
-        $url_args = '?search_collection=3&search_cycle='.$cycle->id;
-        return view('corpus.collection.3.texts',
-                compact('page_title', 'texts', 'url_args'));
-        
+        $page_title = trans('corpus.cycle') . ': ' . $cycle->name;
+        $url_args = '?search_collection=3&search_cycle=' . $cycle->id;
+        return view(
+            'corpus.collection.3.texts',
+            compact('page_title', 'texts', 'url_args')
+        );
     }
-    
-    public function karelianRunes() {
+
+    public function karelianRunes()
+    {
         return Redirect::to('/corpus/collection/2');
     }
-    
-    public function karelianLegends() {
+
+    public function karelianLegends()
+    {
         return Redirect::to('/corpus/collection/3');
     }
-    
-    public function predictionMotives() {
+
+    public function predictionMotives()
+    {
         $genre_id = Collection::getCollectionGenres(3);
-//        $lang_id = Collection::getCollectionLangs(3);
+        //        $lang_id = Collection::getCollectionLangs(3);
         $motypes = Motype::whereGenreId($genre_id)->orderBy('code')->get();
-        return view('corpus.collection.3.motives',
-                compact('motypes'));        
+        return view(
+            'corpus.collection.3.motives',
+            compact('motypes')
+        );
     }
-    
-    public function predictionTextsForMotive($motive_id) {
+
+    public function predictionTextsForMotive($motive_id)
+    {
         $motive = Motive::find($motive_id);
         $lang_id = Collection::getCollectionLangs(3);
         $texts = $motive->texts()->whereIn('lang_id', $lang_id)->get();
-        $page_title = trans('corpus.motive'). ': '. $motive->full_name;
-        $url_args = '?search_collection=3&search_motive='.$motive->id;
+        $page_title = trans('corpus.motive') . ': ' . $motive->full_name;
+        $url_args = '?search_collection=3&search_motive=' . $motive->id;
         $back_link = ['/corpus/collection/3/motives', trans('collection.motive_index')];
-        return view('corpus.collection.3.texts',
-                compact('back_link', 'page_title', 'texts', 'url_args'));
-        
+        return view(
+            'corpus.collection.3.texts',
+            compact('back_link', 'page_title', 'texts', 'url_args')
+        );
     }
 }
